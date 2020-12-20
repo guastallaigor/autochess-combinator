47e88e23f671634176a7f91622b23773b3fa2419
Igor Guastalla <limaguastallaigor@gmail.com>
2020-12-20 19:09:58 -0300

    fix: local images

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ module.exports = withPWA({
   pwa: {
     dest: "public",
   },
-  images: {
-    domains: ["static.ilongyuan.cn"],
-  },
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
