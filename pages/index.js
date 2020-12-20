bf85eccfcea2877bc143c8f09f554f611cd3e301
Igor Guastalla <limaguastallaigor@gmail.com>
2020-12-20 11:52:49 -0300

    feat: add all icons, layout responsive

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,37 @@ import tw, { styled } from "twin.macro";
 // import axios from "axios";
 import Image from "next/image";
 
+const prefixMap = `flex flex-col flex-nowrap transition-all duration-300 ease-in-out cursor-pointer hover:shadow-xl relative top-0 hover:-top-2 hover:opacity-80 w-48 overflow-hidden rounded-lg bg-gradient-to-r`;
+const styleMap = {
+  Common: tw`${prefixMap} from-gray-200 to-gray-400`,
+  default: tw`${prefixMap} from-gray-200 to-gray-400`,
+};
+const getStyleName = ({ quality }) => styleMap[quality] || styleMap.default;
+const Container = styled.div(getStyleName);
+
 const Home = ({ data }) => {
   const getFromArrayOrString = (payload) => {
     return Array.isArray(payload) && payload.length ? payload[0] : payload;
   };
-  const getCardTypeIconImage = (cardType) => {
-    const typeIcon = getFromArrayOrString(cardType);
-    const image = {
-      Beast: "/beast.png",
-    }[typeIcon];
-    return image || "/beast.png";
-  };
 
   const getCardCategoryIconImage = (cardCategory) => {
     const categoryIcon = getFromArrayOrString(cardCategory);
-    const image = {
-      Druid: "/druid.png",
-    }[categoryIcon];
-    return image || "/druid.png";
+    return categoryIcon ? `/${categoryIcon.toLowerCase()}.png` : "/none.png";
   };
 
-  const prefixMap = `flex flex-col flex-nowrap transition-all duration-300 ease-in-out cursor-pointer hover:shadow-xl relative top-0 hover:-top-2 hover:opacity-80 w-48 overflow-hidden rounded-lg bg-gradient-to-r`;
-  const styleMap = {
-    Common: tw`${prefixMap} from-gray-200 to-gray-400`,
-    default: tw`${prefixMap} from-gray-200 to-gray-400`,
+  const getCardTypeIconImage = (cardType) => {
+    const typeIcon = getFromArrayOrString(cardType);
+    return typeIcon ? `/${typeIcon.toLowerCase()}.png` : "/none.png";
   };
-  const getStyleName = ({ quality }) => styleMap[quality] || styleMap.default;
-  const Container = styled.div(getStyleName);
+
+  // 192x352.73
 
   return (
-    <div tw="flex justify-center items-center h-full w-full">
-      <div tw="grid grid-cols-5 gap-7 rounded-sm">
+    <div tw="flex flex-row justify-center items-center">
+      <div
+        tw="grid max-w-6xl w-full h-full gap-7 justify-items-center"
+        className="grid-cols"
+      >
         {data.map((it) => {
           return (
             <Container
@@ -40,9 +40,9 @@ const Home = ({ data }) => {
               key={it.resource_code}
             >
               <div tw="flex flex-row flex-nowrap items-center justify-between p-2 bg-white mb-3">
-                <span tw="font-medium text-gray-700">
+                <div tw="font-medium text-gray-700 truncate text-sm">
                   {it.fields_data.name}
-                </span>
+                </div>
                 <div tw="flex flex-row flex-nowrap items-center">
                   <Image
                     src="/coin.png"
@@ -66,25 +66,51 @@ const Home = ({ data }) => {
                 quality={70}
               />
               <div tw="flex flex-row flex-nowrap relative justify-center mt-3">
-                <div tw="bg-gray-900 absolute h-9 w-10 top-1 left-0 rounded-full text-center">
-                  <Image
-                    src={getCardTypeIconImage(it.fields_data.cardType)}
-                    alt="Chess Icon image"
-                    layout="fixed"
-                    tw="overflow-hidden"
-                    width={32}
-                    height={32}
-                    quality={70}
-                  />
-                </div>
-                <div tw="flex flex-col flex-1 text-center text-base">
-                  <div tw="bg-gray-900 w-full text-gray-100">Beast</div>
-                  <div tw="bg-yellow-600 w-full text-gray-100">Druid</div>
+                {it.fields_data.category.length > 1 ? (
+                  <div tw="bg-gray-900 absolute w-10 top-1 left-0 rounded-3xl flex flex-col items-center text-center gap-1 h-12">
+                    {it.fields_data.category.map((category, idx) => {
+                      return (
+                        <Image
+                          key={category + idx}
+                          src={getCardCategoryIconImage(category)}
+                          alt="Chess Icon Category Image"
+                          layout="fixed"
+                          tw="overflow-hidden"
+                          width={20}
+                          height={20}
+                          quality={70}
+                        />
+                      );
+                    })}
+                  </div>
+                ) : (
+                  <div tw="bg-gray-900 absolute h-9 w-10 top-1 left-0 rounded-full text-center">
+                    <Image
+                      src={getCardCategoryIconImage(it.fields_data.category)}
+                      alt="Chess Icon Category Image"
+                      layout="fixed"
+                      width={32}
+                      height={32}
+                      quality={70}
+                    />
+                  </div>
+                )}
+                <div tw="flex flex-col flex-1 text-center text-sm items-center">
+                  <div tw="bg-gray-900 w-full text-gray-100 py-1">
+                    {it.fields_data.category.length
+                      ? it.fields_data.category.join(", ")
+                      : "None"}
+                  </div>
+                  <div tw="bg-yellow-600 w-full text-gray-100 py-1">
+                    {it.fields_data.cardType.length
+                      ? it.fields_data.cardType.join(", ")
+                      : "None"}
+                  </div>
                 </div>
                 <div tw="bg-yellow-600 absolute right-0 bottom-1 rounded-full text-center">
                   <Image
-                    src={getCardCategoryIconImage(it.fields_data.category)}
-                    alt="Chess Icon image"
+                    src={getCardTypeIconImage(it.fields_data.cardType)}
+                    alt="Chess Icon Type Image"
                     layout="fixed"
                     width={32}
                     height={32}
