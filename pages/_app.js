2e7d45f72e14ba5b6c856120f1b46f3b4a5fad20
Igor Guastalla <limaguastallaigor@gmail.com>
2020-12-22 11:07:48 -0300

    feat: add report to webvitals with quickmetrics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,38 @@ import { GlobalStyles } from "twin.macro";
 import Head from "next/head";
 import "../styles/globals.css";
 
+const sendMetric = ({ name, value }) => {
+  const url = `https://qckm.io?m=${name}&v=${value}&k=${process.env.NEXT_PUBLIC_QUICK_METRICS_API_KEY}`;
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url);
+  } else {
+    fetch(url, { method: "POST", keepalive: true });
+  }
+};
+
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case "FCP":
+      sendMetric(metric);
+      break;
+    case "LCP":
+      sendMetric(metric);
+      break;
+    case "CLS":
+      sendMetric(metric);
+      break;
+    case "FID":
+      sendMetric(metric);
+      break;
+    case "TTFB":
+      sendMetric(metric);
+      break;
+    default:
+      break;
+  }
+}
+
 const App = ({ Component, pageProps }) => (
   <>
     <Head>
